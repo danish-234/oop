47e740dec87064633e5cb0d286c69a66466e15b0
danish-234 <56949614+danish-234@users.noreply.github.com>
2019-11-24 23:58:24 +0500

    Add files via upload

---
+++ 2,77.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int a,b;
+	cout<<"enter value of a :" ;
+	cin>>a;
+	cout<<"enter value of b:" ;
+   cin>>b;
+   if (a>b){
+   	cout<<"B is smaller"<</"n";
+   }
+
+if (b>a){
+	cout<<"A is smaller"<</"n";
+		
+}
+if (a!=b ||b!=a)
+{
+	
+	cout<<" these number are not equal";
+}
+ return 0;
+
+
