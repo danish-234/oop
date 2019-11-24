69e1ba83e42f33b2cc9f7e0d3700eac7796601db
danish-234 <56949614+danish-234@users.noreply.github.com>
2019-11-25 01:29:22 +0500

    Add files via upload

---
+++ 2.29.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using name space std;
+int main()
+{
+	int a,b,c;
+	int sum, avg, prod;
+	  cout<<"enter value of a":;
+	cin>>a;
+	cout<<"enter value of b":;
+	cin>>b;
+	cout<<"enter value of c":;
+	cin>>c;
+	sum=a+b+c;
+	avg+a+b+C/3;
+	product=a*b*c;
+	cout<<"sum is"<<sum is<<"/n";
+	cout"avgerage is "<<avg<<"/n";
+	cout<<product is "<<product<<"/n";
+	if(a>b&&a>c)
+	{
+		
+		cout<<"largest is"<<a<<"/n;
+		
+	}
+	
+	if (b>a&&>c)
+	{
+		
+		cout<<"largest is "<<b<<"/n";
+		
+	}
+	
+	if (c>a&&b>c)
+	{
+		
+		cout<<"largest is "<<c<<"/n;
+	}
+	
+}
+if (b<a&&b<c)
+
+cout<<"smallest is "<<a<<"/n";
+}
+
+if (c<a&&c<b)
+{
+	
+	cout<<"smallest is"<<b<<"/n";
+	if(c>a&&c<b)
+	{
+		
+		
+		cout<<"smallest is "<<c<<"/n";
+		
+	}
+		}
+	
+}
+return 0;
