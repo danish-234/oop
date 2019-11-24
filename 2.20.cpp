5fc478b4de384b017eca81b10544dbc6fee34244
danish-234 <56949614+danish-234@users.noreply.github.com>
2019-11-25 01:35:39 +0500

    Add files via upload

---
+++ 2.20.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	int r,d,a,c;
+	const int pi=3.14159;
+	cout<<"enter radiyus:";
+	cin>>r;
+	d=2*r;
+	a=pi*r^2;
+	c=2*pi*r;
+	cout<<"diameter of circle"=<<d<<"/n";
+	cout<<"area of circle"=<<a<<"/n";
+	cout<<"circumstance of circle= "<<c<</"n";
+	return 0;
+	
+}
+
